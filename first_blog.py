c736ac393bca2bbac4c7cc49d7d782db0ff0fc4d
Tushar Sharma <tushar.sharma118@gmail.com>
2019-02-18 17:10:04 +0530

    Added code for html template layouts and some css
    Learned to return template layouts and using url_for() method

--- first_blog.py
+++ first_blog.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 posts = [
     {
-        'author' : 'Helix',
-        'title' : 'First Post',
-        'content' : 'This is my first Post!',
-        'dated' : 'February 12, 2019'
+        'author': 'Helix',
+        'title': 'First Post',
+        'content': 'This is my first Post!',
+        'dated': 'February 12, 2019'
     },
     {
         'author': 'Helix Sama',
@@ -17,16 +17,18 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def home():
-    title = 'MUAHAHAHHAAH!'
-    return render_template('home.html', title = title)
+    title = 'Home!'
+    return render_template('home.html', title=title, posts= posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
